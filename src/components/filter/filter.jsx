66d6e05e1d0e5bfba120e80049cf174fea9804ac
TomiisinA <tomisinajayi@gmail.com>
2025-01-31 13:41:06 +0100

    new

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -14,25 +14,52 @@ function Filter() {
         </div>
         <div className="bottom">
           <div className="item">
-            <label htmlFor="city">Location</label>
-            <input type="text" id="city" placeholder="city location" />
+            <label htmlFor="type">Type</label>
+            <select name="type" id="type">
+              <option value="any">Any</option>
+              <option value="buy">Buy</option>
+              <option value="rent">Rent</option>
+            </select>
           </div>
           <div className="item">
-            <label htmlFor="city">Location</label>
-            <input type="text" id="city" placeholder="city location" />
+            <label htmlFor="property">Property</label>
+            <select name="" id="">
+              <option value="any">Any</option>
+              <option value="house">House</option>
+              <option value="condo">Condo</option>
+              <option value="value">Land</option>
+            </select>
           </div>
           <div className="item">
-            <label htmlFor="city">Location</label>
-            <input type="text" id="city" placeholder="city location" />
+            <label htmlFor="minPrice">MinPrice</label>
+            <input
+              type="number"
+              id="minPrice"
+              name="minprice"
+              placeholder="any"
+            />
           </div>
           <div className="item">
-            <label htmlFor="city">Location</label>
-            <input type="text" id="city" placeholder="city location" />
+            <label htmlFor="bedroom">MaxPrice</label>
+            <input
+              type="text"
+              id="maxPrice"
+              name="maxPrice"
+              placeholder="any"
+            />
           </div>
           <div className="item">
-            <label htmlFor="city">Location</label>
-            <input type="text" id="city" placeholder="city location" />
+            <label htmlFor="bedroom">Bedroom</label>
+            <input
+              type="text"
+              id="bedroom"
+              placeholder="bedroom"
+              name="bedroom"
+            />
           </div>
+          <button>
+            <img src="/search.png" alt="" />
+          </button>
         </div>
       </div>
     </div>
