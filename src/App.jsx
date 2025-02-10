bb9a9329e2e97c768315d49d128361b1a5383483
TomiisinA <tomisinajayi@gmail.com>
2025-02-10 16:25:12 +0100

    profile page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profile/profilePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -23,6 +24,10 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
         },
       ],
     },
